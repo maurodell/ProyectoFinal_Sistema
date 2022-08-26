4212ed1b3e94ef5769e3f68ab7d02b9914a0414e
maurodell <delloliomauro@gmail.com>
2022-08-26 13:08:09 -0300

    first commit

---
+++ Abstraccion/Repositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraccion
+{
+    public interface Repositorio<T> where T : IEntidad
+    {
+        bool Crear(T Parametro);
+        bool Baja(T Parametro);
+        bool Alta(T Parametro);
+        bool Modificar(T Parametro);
+        List<T> Listar(T Parametro);
+        T Leer(int Parametro);
+    }
+}
